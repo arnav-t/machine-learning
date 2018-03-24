fa17bb8716d364930af1203012343be1f654e173
Arnav Tiwari <34343421+arnav-t@users.noreply.github.com>
2018-03-24 05:14:38 +0000

    Add files via upload

--- linear_regression.py
+++ linear_regression.py
@@ -1,8 +1,8 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-iters = 1500
-alpha = 0.01
+iters = 500
+alpha = 2
 errors = np.zeros((0,0))
 
 def loadData(path):
@@ -21,17 +21,27 @@ def gradientDescent():
 	newTheta = theta - (alpha/rows)*np.matmul(np.transpose(X),h()-Y)
 	return newTheta
 
+def normalize(A):
+	Amean = np.mean(A)
+	Amax = np.max(A)
+	Amin = np.min(A)
+	An = (A - Amean)/(Amax - Amin)
+	return An
+
+
 data = loadData('ex1data2.txt')
 rows = data.shape[0]
 cols = data.shape[1]
 
 Y = np.zeros((rows,1))
 Y[:,0] = data[:,cols-1]
+Y = normalize(Y)
 
 X = np.ones(data.shape)
 i = 0
 while(i < cols-1):
 	X[:,i+1] = data[:,i]
+	X[:,i+1] = normalize(X[:,i+1])
 	i += 1
 
 theta = np.zeros((cols,1))
@@ -40,11 +50,14 @@ print('Performing gradient descent...')
 
 i = 1
 while i <= iters:
-	print('Iteration: ' + str(i))
-	errors = np.insert(errors,i-1,np.sum(J()))
+	print('Iteration: ' + str(i), end = ' ')
+	errors = np.insert(errors,i-1,J()[0,0])
+	print('Error: ' + str(errors[i-1]))
 	theta = gradientDescent()
 	i += 1
 	
+print('Plotting...')
+
 plt.subplot(cols,1,1)
 x = np.zeros((1,iters))
 x[0,:] = np.arange(1,iters+1)
@@ -55,21 +68,17 @@ plt.title('Error function')
 plt.ylabel('Error')
 plt.xlabel('Iterations')
 
-print('Plotting...')
-
 i = 0
 while i < cols - 1:
-	plt.subplot(cols,i+2,1)
-	plt.plot(X[:,i],Y,'rx')
-	x = np.zeros((1,2))
-	x[0,0] = np.min(X[:,i])
-	x[0,1] = np.max(X[:,i])
-	y= np.zeros((1,2))
-	y[0,0] = theta[0,0] + theta[i,0]*x[0,0]
-	y[0,1] = theta[0,0] + theta[i,0]*x[0,1]
+	plt.subplot(cols,1,i+2)
+	plt.plot(X[:,i+1],Y,'rx')
+	y= np.zeros((rows,1))
+	y[:,0] = theta[0,0] + theta[i+1,0]*X[:,i+1]
+	x = np.zeros((rows,1))
+	x[:,0] = X[:,i+1]
 	plt.plot(x,y)
 	plt.title('Data Set ' + str(i+1))
-	plt.ylabel('Y' + str(i+1))
+	plt.ylabel('Y')
 	plt.xlabel('X' + str(i+1))
 	i += 1
 
